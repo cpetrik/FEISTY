f2c1947cf466c4f27d546bf40dbf1b14056b1c01
Colleen Petrik <colleenpetrik@gmail.com>
2020-08-30 13:02:47 -0700

    New FishMIP runs w/ empirical HPloss

--- CODE/FishMIP/Make.m
+++ CODE/FishMIP/Make.m
@@ -4,16 +4,16 @@ clear all
 close all
 
 %%%%!! EXPERIMENTS
-spinup_ipsl = false;
-spinup_gfdl = false;
-pre_industrial_ipsl = true;
-pre_industrial_gfdl = true;
+spinup_ipsl = true;
+spinup_gfdl = true;
+pre_industrial_ipsl = false;
+pre_industrial_gfdl = false;
 historic_ipsl = false;
 historic_gfdl = false;
-ssp126_ipsl = true;
-ssp126_gfdl = true;
-ssp585_ipsl = true;
-ssp585_gfdl = true;
+ssp126_ipsl = false;
+ssp126_gfdl = false;
+ssp585_ipsl = false;
+ssp585_gfdl = false;
 
 forecast_cesm = false;
 forecast_gfdl = false;
@@ -24,34 +24,44 @@ npp_cont_gfdl = false;
 
 tic
 if spinup_ipsl
-    Spinup_pristine_ipsl()
+%     Spinup_pristine_ipsl()
+    Spinup_pristine_empHP_ipsl()
 end
 if spinup_gfdl
-    Spinup_pristine_gfdl()
+%     Spinup_pristine_gfdl()
+    Spinup_pristine_empHP_gfdl()
 end
 if pre_industrial_ipsl
-    Preindust_pristine_ipsl()
+%     Preindust_pristine_ipsl()
+    Preindust_pristine_empHP_ipsl()
 end
 if pre_industrial_gfdl
-    Preindust_pristine_gfdl()
+%     Preindust_pristine_gfdl()
+    Preindust_pristine_empHP_gfdl()
 end
 if historic_ipsl
-    Historic_pristine_ipsl()
+%     Historic_pristine_ipsl()
+    Historic_pristine_empHP_ipsl()
 end
 if historic_gfdl
-    Historic_pristine_gfdl()
+%     Historic_pristine_gfdl()
+    Historic_pristine_empHP_gfdl()
 end
 if ssp126_ipsl
-    SSP126_pristine_ipsl()
+%     SSP126_pristine_ipsl()
+    SSP126_pristine_empHP_ipsl()
 end
 if ssp126_gfdl
-    SSP126_pristine_gfdl()
+%     SSP126_pristine_gfdl()
+    SSP126_pristine_empHP_gfdl()
 end
 if ssp585_ipsl
-    SSP585_pristine_ipsl()
+%     SSP585_pristine_ipsl()
+    SSP585_pristine_empHP_ipsl()
 end
 if ssp585_gfdl
-    SSP585_pristine_gfdl()
+%     SSP585_pristine_gfdl()
+    SSP585_pristine_empHP_gfdl()
 end
 
 
---
+++ CODE/FishMIP/get_ESM_empHPloss.m
@@ -0,0 +1,18 @@
+%%% Get CESM data
+function ENVR = get_ESM_empHPloss(ESM,GRD,param,DY)
+
+    %% Get data
+    ENVR.Tp(:,1)  = ESM.Tp(param.ID,DY);
+    ENVR.Tb(:,1)  = ESM.Tb(param.ID,DY);
+    ENVR.Zm(:,1)  = ESM.Zm(param.ID,DY);
+    ENVR.det(:,1) = ESM.det(param.ID,DY);
+%     ENVR.dZm(:,1) = ESM.dZm(param.ID,DY);
+    ENVR.fZl(:,1) = zeros(param.NX,1);
+    ENVR.fB(:,1)  = zeros(param.NX,1);
+    ENVR.H(:,1)   = GRD.Z(param.ID);
+%     ENVR.A(:,1)   = GRD.AREA(param.ID);
+
+% HP loss is an empirical fitted fn of biomass and temp
+    ENVR.dZm(:,1) = 10 .^ (-2.925 + 1.964.*log10(ENVR.Zm(:,1)+eps) + 1.958e-2.*ENVR.Tp(:,1));
+    
+end
