18681adf92404722d24fedf5f878c7be7b98e70e
Colleen Petrik <colleenpetrik@gmail.com>
2021-12-21 11:44:50 -0800

    Run with CESM DPLE Y2015 for CCATCH

---
+++ CODE/FishMIP/mfiles_FishMIP/NPP_Temp_sens/netcdf_read_hist_bio_CCLME_sample.m
@@ -0,0 +1,146 @@
+% FEISTY output at all locations
+
+clear all
+close all
+
+cfile = 'Dc_enc70-b200_m4-b175-k086_c20-b250_D075_J100_A050_Sm025_nmort1_BE08_noCC_RE00100';
+
+fpath=['/Volumes/MIP/FEISTY_output/FishMIP/CESM1-BEC/' cfile '/'];
+
+%% SP
+ncid = netcdf.open([fpath 'Historic_pristine_sml_p.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+[ni,nt] = size(biomass);
+
+SP.bio = biomass;
+clear biomass
+
+% SF
+ncid = netcdf.open([fpath 'Historic_pristine_sml_f.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+SF.bio = biomass(:,1:nt);
+clear biomass 
+
+% SD
+ncid = netcdf.open([fpath 'Historic_pristine_sml_d.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+SD.bio = biomass;
+clear biomass 
+
+% MP
+ncid = netcdf.open([fpath 'Historic_pristine_med_p.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+MP.bio = biomass;
+clear biomass
+
+% MF
+ncid = netcdf.open([fpath 'Historic_pristine_med_f.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+MF.bio = biomass;
+clear biomass
+
+% MD
+ncid = netcdf.open([fpath 'Historic_pristine_med_d.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+MD.bio = biomass;
+clear biomass
+
+% LP
+ncid = netcdf.open([fpath 'Historic_pristine_lrg_p.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+LP.bio = biomass;
+clear biomass
+
+% LD
+ncid = netcdf.open([fpath 'Historic_pristine_lrg_d.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+LD.bio = biomass;
+clear biomass
+
+% Benthic material
+ncid = netcdf.open([fpath 'Historic_pristine_bent.nc'],'NC_NOWRITE');
+[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+for i = 1:nvars
+    varname = netcdf.inqVar(ncid, i-1);
+    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+    eval([ varname '(' varname ' == 99999) = NaN;']);
+end
+netcdf.close(ncid);
+
+Bent.bio = biomass;
+clear biomass
+
+%% Fish-MIP OUTPUTS =================================================
+t=time;
+mo=t/12;
+mo=mo+1850;
+yr20=find(mo>2000);  
+
+allF = SF.bio(:,yr20) + MF.bio(:,yr20);
+allP = SP.bio(:,yr20) + MP.bio(:,yr20) + LP.bio(:,yr20);
+allD = SD.bio(:,yr20) + MD.bio(:,yr20) + LD.bio(:,yr20);
+allB = Bent.bio(:,yr20);
+
+save([fpath 'CESM_Hist_2000_2006_empHP_fishMIP_outputs_monthly_' cfile '.mat'],'time','mo',...
+    'allF','allD','allP','allB','yr20');
+
+
+
+
+
