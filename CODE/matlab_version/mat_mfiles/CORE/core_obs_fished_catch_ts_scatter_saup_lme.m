105a8bc608ef528156d55fcf3e7553fa429a620c
Colleen Petrik <colleenpetrik@gmail.com>
2022-06-16 13:18:55 -0700

    CORE obs fishing w/correct yrs

--- CODE/matlab_version/mat_mfiles/CORE/core_obs_fished_catch_ts_scatter_saup_lme.m
+++ CODE/matlab_version/mat_mfiles/CORE/core_obs_fished_catch_ts_scatter_saup_lme.m
@@ -76,6 +76,29 @@ xlabel('SAUP')
 title('All')
 print('-dpng',[ppath 'CORE_obs_fished_lme_scatter_all_yrs.png'])
 
+%% log10 Global
+figure(2)
+subplot(2,2,1)
+plot(log10(Flme_scatch_all(:)),log10(Flme_fcatch_all(:)),'.r','MarkerSize',20)
+ylabel('log_1_0 FEISTY catch (MT km^-^2)')
+title('Forage')
+
+subplot(2,2,2)
+plot(log10(Plme_scatch_all(:)),log10(Plme_fcatch_all(:)),'.b','MarkerSize',20)
+title('Large pelagic')
+
+subplot(2,2,3)
+plot(log10(Dlme_scatch_all(:)),log10(Dlme_fcatch_all(:)),'.','color',[0 0.6 0],'MarkerSize',20)
+xlabel('SAUP')
+ylabel('log_1_0 FEISTY catch (MT km^-^2)')
+title('Demersal')
+
+subplot(2,2,4)
+plot(log10(slme_scatch_all(:)),log10(Alme_fcatch_all(:)),'.k','MarkerSize',20)
+xlabel('SAUP')
+title('All')
+print('-dpng',[ppath 'CORE_obs_fished_lme_scatter_all_yrs_log10.png'])
+
 %% Scatter by LME
 nrows=11;
 ncols=6;
@@ -105,7 +128,6 @@ for m = 1:nrows
         i=i+1;
         subplot('position',pos(m,:,n))
         plot(Flme_scatch_all(i,:),Flme_fcatch_all(i,:),'.r','MarkerSize',10); hold on;
-        xlim([0 2])
         title(num2str(i))
         if (i<61)
             set(gca,'XTickLabel',[])
@@ -148,7 +170,7 @@ print('-dpng',[ppath 'CORE_byLME_obs_fished_scatter_all_yrs_D.png'])
 
 %% Time series Plots 
 % Global
-figure(6)
+figure(7)
 subplot(2,2,1)
 plot(years,sum(Flme_scatch_all),'color',[0.5 0.5 0.5],'LineWidth',2); hold on
 plot(years,sum(Flme_fcatch_all),'r','LineWidth',2)
@@ -173,5 +195,101 @@ plot(years,sum(Alme_fcatch_all),'k','LineWidth',2)
 xlabel('year')
 title('All')
 print('-dpng',[ppath 'CORE_obs_fished_sumlme_ts.png'])
+
+%% log10 Global
+figure(8)
+subplot(2,2,1)
+plot(years,log10(sum(Flme_scatch_all)),'color',[0.5 0.5 0.5],'LineWidth',2); hold on
+plot(years,log10(sum(Flme_fcatch_all)),'r','LineWidth',2)
+ylabel('log_1_0 FEISTY catch (MT km^-^2)')
+title('Forage')
+
+subplot(2,2,2)
+plot(years,log10(sum(Plme_scatch_all)),'color',[0.5 0.5 0.5],'LineWidth',2); hold on
+plot(years,log10(sum(Plme_fcatch_all)),'b','LineWidth',2)
+title('Large pelagic')
+
+subplot(2,2,3)
+plot(years,log10(sum(Dlme_scatch_all)),'color',[0.5 0.5 0.5],'LineWidth',2); hold on
+plot(years,log10(sum(Dlme_fcatch_all)),'color',[0 0.6 0],'LineWidth',2)
+xlabel('year')
+ylabel('log_1_0 FEISTY catch (MT km^-^2)')
+title('Demersal')
+
+subplot(2,2,4)
+plot(years,log10(sum(slme_scatch_all)),'color',[0.5 0.5 0.5],'LineWidth',2); hold on
+plot(years,log10(sum(Alme_fcatch_all)),'k','LineWidth',2)
+xlabel('year')
+title('All')
+print('-dpng',[ppath 'CORE_obs_fished_sumlme_ts_log10.png'])
+
+%% TS by LME
+% all 
+figure('Units','inches','Position',[1 3 6.5 8.5]);
+i=0;
+for m = 1:nrows
+    for n = 1:ncols
+        i=i+1;
+        subplot('position',pos(m,:,n))
+        plot(years,slme_scatch_all(i,:),'color',[0.5 0.5 0.5],'LineWidth',1); hold on
+        plot(years,Alme_fcatch_all(i,:),'k','LineWidth',1)
+        title(num2str(i))
+        if (i<61)
+            set(gca,'XTickLabel',[])
+        end
+    end
+end
+print('-dpng',[ppath 'CORE_byLME_obs_fished_ts_Allfish.png'])
+
+%% F
+figure('Units','inches','Position',[1 3 6.5 8.5]);
+i=0;
+for m = 1:nrows
+    for n = 1:ncols
+        i=i+1;
+        subplot('position',pos(m,:,n))
+        plot(years,Flme_scatch_all(i,:),'color',[0.5 0.5 0.5],'LineWidth',1); hold on
+        plot(years,Flme_fcatch_all(i,:),'r','LineWidth',1)
+        title(num2str(i))
+        if (i<61)
+            set(gca,'XTickLabel',[])
+        end
+    end
+end
+print('-dpng',[ppath 'CORE_byLME_obs_fished_ts_F.png'])
+
+%% P
+figure('Units','inches','Position',[1 3 6.5 8.5]);
+i=0;
+for m = 1:nrows
+    for n = 1:ncols
+        i=i+1;
+        subplot('position',pos(m,:,n))
+        plot(years,Plme_scatch_all(i,:),'color',[0.5 0.5 0.5],'LineWidth',1); hold on
+        plot(years,Plme_fcatch_all(i,:),'b','LineWidth',1)
+        title(num2str(i))
+        if (i<61)
+            set(gca,'XTickLabel',[])
+        end
+    end
+end
+print('-dpng',[ppath 'CORE_byLME_obs_fished_ts_P.png'])
+
+%% D
+figure('Units','inches','Position',[1 3 6.5 8.5]);
+i=0;
+for m = 1:nrows
+    for n = 1:ncols
+        i=i+1;
+        subplot('position',pos(m,:,n))
+        plot(years,Dlme_scatch_all(i,:),'color',[0.5 0.5 0.5],'LineWidth',1); hold on
+        plot(years,Dlme_fcatch_all(i,:),'color',[0 0.6 0],'LineWidth',1)
+        title(num2str(i))
+        if (i<61)
+            set(gca,'XTickLabel',[])
+        end
+    end
+end
+print('-dpng',[ppath 'CORE_byLME_obs_fished_ts_D.png'])
 
 
