3f8cce9728692519eb32d43c09172930757d81fe
Colleen <cpetrik@users.noreply.github.com>
2025-04-28 17:52:39 -0700

    more Lfracs

--- CODE/FishMIP/mfiles_FishMIP/ext2300_mfiles/daily_interp_cesm_hist_rockfish_Lfrac85.m
+++ CODE/FishMIP/mfiles_FishMIP/ext2300_mfiles/daily_interp_cesm_hist_rockfish_Lfrac85.m
@@ -17,6 +17,8 @@ ppath='/project/Feisty/Fish-MIP/CMIP6/CESM2-WACCM/preindust/';
 load([fpath 'cesm2_hist_temp_btm_monthly_1850_2014.mat'],'temp_btm');
 load([fpath 'cesm2_hist_temp_150_monthly_1850_2014.mat'],'temp_150');
 load([fpath 'cesm2_hist_zooc_150_monthly_1850_2014.mat'],'zooc_150','units_vint');
+load([fpath 'cesm2_hist_phyc_150_monthly_1850_2014.mat'],'phyc_150');
+load([fpath 'cesm2_hist_diat_150_monthly_1850_2014.mat'],'diat_150');
 load([fpath 'cesm2_hist_det_monthly_1850_2014.mat']); %,'det'
 
 load([ppath 'cesm2-waccm_r1i1p1f1_picontrol_deptho_60arcmin_global_fx.mat'],'deptho')
@@ -25,7 +27,19 @@ load([ppath 'cesm2-waccm_r1i1p1f1_picontrol_deptho_60arcmin_global_fx.mat'],'dep
 temp_btm(temp_btm > 1.0e19) = nan;
 temp_150(temp_150 > 1.0e19) = nan;
 zooc_150(zooc_150 > 1.0e19) = nan;
+phyc_150(phyc_150 > 1.0e19) = nan;
+diat_150(diat_150 > 1.0e19) = nan;
 det(det > 1.0e19) = nan;
+
+%% Calc zmeso from diat frac
+Lfrac = double(diat_150) ./ double(phyc_150);
+Lfrac(Lfrac>1) = 1.0;
+Lfrac(Lfrac<0) = 0.0;
+
+zmeso_150 = (Lfrac .* double(zooc_150)) + (0.85 * (1-Lfrac) .* double(zooc_150));
+
+save([fpath 'cesm2_hist_zmeso_150_Lfrac85_monthly_1850_2014.mat'],'zmeso_150',...
+    'Lfrac','units_vint','time','yr')
 
 %%
 mos = length(time);
@@ -42,7 +56,7 @@ Tdays=1:365;
 %% test that all same orientation
 test1 = squeeze(double(temp_150(:,:,70)));
 test2 = squeeze(double(temp_btm(:,:,70)));
-test3 = squeeze(double(zooc_150(:,:,70)));
+test3 = squeeze(double(zmeso_150(:,:,70)));
 test4 = squeeze(double(det(:,:,70)));
 
 % figure
@@ -60,7 +74,7 @@ test4 = squeeze(double(det(:,:,70)));
 
 %% index of water cells
 %make GRD in another file later
-% load('/Volumes/petrik-lab/Feisty/Fish-MIP/CMIP6/IPSL/Data_grid_cesm.mat','GRD');
+% load('/project/Feisty/Fish-MIP/CMIP6/IPSL/Data_grid_cesm.mat','GRD');
 % WID = GRD.ID;
 % NID = GRD.N;
 
@@ -87,7 +101,7 @@ for y = 100 %:nyrs
 
     Tp = double(temp_150(:,:,range));
     Tb = double(temp_btm(:,:,range));
-    Zm = double(zooc_150(:,:,range));
+    Zm = double(zmeso_150(:,:,range));
     Det= double(det(:,:,range));
 
     % setup FEISTY data files
@@ -138,7 +152,7 @@ for y = 100 %:nyrs
     ESM.det = D_det;
 
     % save
-    save([fpath 'Data_cesm_hist_daily_',num2str(ytime),'_zooc.mat'], 'ESM');
+    save([fpath 'Data_cesm_hist_daily_',num2str(ytime),'_Lfrac85.mat'], 'ESM');
 
 
 end
