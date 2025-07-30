28e06863aaa1a79b6b1633b40f32a09178cc8c24
cpetrik <cpetrik@rockfish.ucsd.edu>
2025-07-29 17:23:46 -0700

    Remove duplicated Advec directory

--- CODE/Advec/Advect_Fish_Code/matlab_functions/zeroCornerMatrix.m
+++
@@ -1,8 +0,0 @@
-function [ output ] = zeroCornerMatrix( input )
-    % Makes the four corners = 0
-    output = input;
-    output(1,1)     = 0;
-    output(1,end)   = 0;
-    output(end,1)   = 0;
-    output(end,end) = 0;
-end
--- CODE/Advec/Advect_Fish_Code/sub_tot_mort.m
+++
@@ -1,9 +0,0 @@
-%%% Update biomass
-function tmort = sub_tot_mort(pred,nmort,fmort)
-    % all inputs are in g g-1 d-1; 
-    % nmort = natural mortality rate
-    % fmort = fishing rate
-    % pred = predation rate
-    
-    tmort = nmort + fmort + pred;
-end
