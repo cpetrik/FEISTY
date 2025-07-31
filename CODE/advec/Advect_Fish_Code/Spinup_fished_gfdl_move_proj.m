dfcebb816db96ba94a95dc991cb25aace3411064
Colleen <cpetrik@users.noreply.github.com>
2025-07-31 13:32:16 -0700

    Just save first 10 d

--- CODE/advec/Advect_Fish_Code/Spinup_fished_gfdl_move_proj.m
+++ CODE/advec/Advect_Fish_Code/Spinup_fished_gfdl_move_proj.m
@@ -83,81 +83,81 @@ netcdf.setDefaultFormat('NC_FORMAT_64BIT');
 
 %% %%%%%%%%%%%%% Setup NetCDF save
 %! Setup netcdf path to store to
-file_sml_f = [fname,'_sml_f.nc'];
-file_sml_p = [fname,'_sml_p.nc'];
-file_sml_d = [fname,'_sml_d.nc'];
-file_med_f = [fname,'_med_f.nc'];
-file_med_p = [fname,'_med_p.nc'];
-file_med_d = [fname,'_med_d.nc'];
-file_lrg_p = [fname,'_lrg_p.nc'];
-file_lrg_d = [fname,'_lrg_d.nc'];
-file_bent  = [fname,'_bent.nc'];
-
-ncidSF = netcdf.create(file_sml_f,'NC_WRITE');
-ncidSP = netcdf.create(file_sml_p,'NC_WRITE');
-ncidSD = netcdf.create(file_sml_d,'NC_WRITE');
-ncidMF = netcdf.create(file_med_f,'NC_WRITE');
-ncidMP = netcdf.create(file_med_p,'NC_WRITE');
-ncidMD = netcdf.create(file_med_d,'NC_WRITE');
-ncidLP = netcdf.create(file_lrg_p,'NC_WRITE');
-ncidLD = netcdf.create(file_lrg_d,'NC_WRITE');
-ncidB  = netcdf.create(file_bent,'NC_WRITE');
-
-%% ! Def vars of netcdf file
-['Defining netcdfs, takes ~15 minutes ... ']
-xy_dim      = netcdf.defDim(ncidSF,'nid',NX);
-time_dim    = netcdf.defDim(ncidSF,'ntime',nt);
-vidbioSF    = netcdf.defVar(ncidSF,'biomass','double',[xy_dim,time_dim]);
-% vidprodSF   = netcdf.defVar(ncidSF,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidSF);
-
-xy_dim      = netcdf.defDim(ncidSP,'nid',NX);
-time_dim    = netcdf.defDim(ncidSP,'ntime',nt);
-vidbioSP    = netcdf.defVar(ncidSP,'biomass','double',[xy_dim,time_dim]);
-% vidprodSP   = netcdf.defVar(ncidSP,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidSP);
-
-xy_dim      = netcdf.defDim(ncidSD,'nid',NX);
-time_dim    = netcdf.defDim(ncidSD,'ntime',nt);
-vidbioSD    = netcdf.defVar(ncidSD,'biomass','double',[xy_dim,time_dim]);
-% vidprodSD   = netcdf.defVar(ncidSD,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidSD);
-
-xy_dim      = netcdf.defDim(ncidMF,'nid',NX);
-time_dim    = netcdf.defDim(ncidMF,'ntime',nt);
-vidbioMF    = netcdf.defVar(ncidMF,'biomass','double',[xy_dim,time_dim]);
-% vidprodMF   = netcdf.defVar(ncidMF,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidMF);
-
-xy_dim      = netcdf.defDim(ncidMP,'nid',NX);
-time_dim    = netcdf.defDim(ncidMP,'ntime',nt);
-vidbioMP    = netcdf.defVar(ncidMP,'biomass','double',[xy_dim,time_dim]);
-% vidprodMP   = netcdf.defVar(ncidMP,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidMP);
-
-xy_dim      = netcdf.defDim(ncidMD,'nid',NX);
-time_dim    = netcdf.defDim(ncidMD,'ntime',nt);
-vidbioMD    = netcdf.defVar(ncidMD,'biomass','double',[xy_dim,time_dim]);
-% vidprodMD   = netcdf.defVar(ncidMD,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidMD);
-
-xy_dim      = netcdf.defDim(ncidLP,'nid',NX);
-time_dim    = netcdf.defDim(ncidLP,'ntime',nt);
-vidbioLP    = netcdf.defVar(ncidLP,'biomass','double',[xy_dim,time_dim]);
-% vidprodLP   = netcdf.defVar(ncidLP,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidLP);
-
-xy_dim      = netcdf.defDim(ncidLD,'nid',NX);
-time_dim    = netcdf.defDim(ncidLD,'ntime',nt);
-vidbioLD    = netcdf.defVar(ncidLD,'biomass','double',[xy_dim,time_dim]);
-% vidprodLD   = netcdf.defVar(ncidLD,'prod','double',[xy_dim,time_dim]);
-netcdf.endDef(ncidLD);
-
-xy_dim     = netcdf.defDim(ncidB,'nid',NX);
-time_dim   = netcdf.defDim(ncidB,'ntime',nt);
-vidbioB    = netcdf.defVar(ncidB,'biomass','double',[xy_dim,time_dim]);
-vidTB      = netcdf.defVar(ncidB,'time','double',time_dim);
-netcdf.endDef(ncidB);
+% file_sml_f = [fname,'_sml_f.nc'];
+% file_sml_p = [fname,'_sml_p.nc'];
+% file_sml_d = [fname,'_sml_d.nc'];
+% file_med_f = [fname,'_med_f.nc'];
+% file_med_p = [fname,'_med_p.nc'];
+% file_med_d = [fname,'_med_d.nc'];
+% file_lrg_p = [fname,'_lrg_p.nc'];
+% file_lrg_d = [fname,'_lrg_d.nc'];
+% file_bent  = [fname,'_bent.nc'];
+% 
+% ncidSF = netcdf.create(file_sml_f,'NC_WRITE');
+% ncidSP = netcdf.create(file_sml_p,'NC_WRITE');
+% ncidSD = netcdf.create(file_sml_d,'NC_WRITE');
+% ncidMF = netcdf.create(file_med_f,'NC_WRITE');
+% ncidMP = netcdf.create(file_med_p,'NC_WRITE');
+% ncidMD = netcdf.create(file_med_d,'NC_WRITE');
+% ncidLP = netcdf.create(file_lrg_p,'NC_WRITE');
+% ncidLD = netcdf.create(file_lrg_d,'NC_WRITE');
+% ncidB  = netcdf.create(file_bent,'NC_WRITE');
+% 
+% %% ! Def vars of netcdf file
+% ['Defining netcdfs, takes ~15 minutes ... ']
+% xy_dim      = netcdf.defDim(ncidSF,'nid',NX);
+% time_dim    = netcdf.defDim(ncidSF,'ntime',nt);
+% vidbioSF    = netcdf.defVar(ncidSF,'biomass','double',[xy_dim,time_dim]);
+% % vidprodSF   = netcdf.defVar(ncidSF,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidSF);
+% 
+% xy_dim      = netcdf.defDim(ncidSP,'nid',NX);
+% time_dim    = netcdf.defDim(ncidSP,'ntime',nt);
+% vidbioSP    = netcdf.defVar(ncidSP,'biomass','double',[xy_dim,time_dim]);
+% % vidprodSP   = netcdf.defVar(ncidSP,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidSP);
+% 
+% xy_dim      = netcdf.defDim(ncidSD,'nid',NX);
+% time_dim    = netcdf.defDim(ncidSD,'ntime',nt);
+% vidbioSD    = netcdf.defVar(ncidSD,'biomass','double',[xy_dim,time_dim]);
+% % vidprodSD   = netcdf.defVar(ncidSD,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidSD);
+% 
+% xy_dim      = netcdf.defDim(ncidMF,'nid',NX);
+% time_dim    = netcdf.defDim(ncidMF,'ntime',nt);
+% vidbioMF    = netcdf.defVar(ncidMF,'biomass','double',[xy_dim,time_dim]);
+% % vidprodMF   = netcdf.defVar(ncidMF,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidMF);
+% 
+% xy_dim      = netcdf.defDim(ncidMP,'nid',NX);
+% time_dim    = netcdf.defDim(ncidMP,'ntime',nt);
+% vidbioMP    = netcdf.defVar(ncidMP,'biomass','double',[xy_dim,time_dim]);
+% % vidprodMP   = netcdf.defVar(ncidMP,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidMP);
+% 
+% xy_dim      = netcdf.defDim(ncidMD,'nid',NX);
+% time_dim    = netcdf.defDim(ncidMD,'ntime',nt);
+% vidbioMD    = netcdf.defVar(ncidMD,'biomass','double',[xy_dim,time_dim]);
+% % vidprodMD   = netcdf.defVar(ncidMD,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidMD);
+% 
+% xy_dim      = netcdf.defDim(ncidLP,'nid',NX);
+% time_dim    = netcdf.defDim(ncidLP,'ntime',nt);
+% vidbioLP    = netcdf.defVar(ncidLP,'biomass','double',[xy_dim,time_dim]);
+% % vidprodLP   = netcdf.defVar(ncidLP,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidLP);
+% 
+% xy_dim      = netcdf.defDim(ncidLD,'nid',NX);
+% time_dim    = netcdf.defDim(ncidLD,'ntime',nt);
+% vidbioLD    = netcdf.defVar(ncidLD,'biomass','double',[xy_dim,time_dim]);
+% % vidprodLD   = netcdf.defVar(ncidLD,'prod','double',[xy_dim,time_dim]);
+% netcdf.endDef(ncidLD);
+% 
+% xy_dim     = netcdf.defDim(ncidB,'nid',NX);
+% time_dim   = netcdf.defDim(ncidB,'ntime',nt);
+% vidbioB    = netcdf.defVar(ncidB,'biomass','double',[xy_dim,time_dim]);
+% vidTB      = netcdf.defVar(ncidB,'time','double',time_dim);
+% netcdf.endDef(ncidB);
 
 %% %%%%%%%%%%%%%%%%%%%% Run the Model
 
