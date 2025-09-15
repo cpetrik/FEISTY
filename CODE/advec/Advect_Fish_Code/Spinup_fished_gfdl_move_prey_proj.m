adacf0ea3da7cf7c31a0dd52be20699a10ba7250
Colleen <cpetrik@users.noreply.github.com>
2025-09-15 15:44:45 -0700

    Rename sim fname to preyconc

--- CODE/advec/Advect_Fish_Code/Spinup_fished_gfdl_move_prey_proj.m
+++ CODE/advec/Advect_Fish_Code/Spinup_fished_gfdl_move_prey_proj.m
@@ -54,7 +54,7 @@ MNTH = [31,28,31,30,31,30,31,31,30,31,30,31];
 %! Create a directory for output
 %opath = '/Volumes/petrik-lab/Feisty/NC/Matlab_new_size/';
 opath = '/project/Feisty/NC/Matlab_new_size/';
-exper = 'Spinup1988_move_prey_v21_dt6h';
+exper = 'Spinup1988_move_preyconc_v21_dt6h';
 [fname,simname,sname] = sub_fname_spin_move_core(param,opath,exper);
 
 %! Storage variables
