a5eb9f15fd8efd199b615e986e25c577e68446a1
Colleen <cpetrik@users.noreply.github.com>
2025-09-22 15:45:46 -0700

    CORE move diff ICs

--- CODE/advec/Advect_Fish_Code/postproc_mfiles/netcdf_read_core_move_fished.m
+++ CODE/advec/Advect_Fish_Code/postproc_mfiles/netcdf_read_core_move_fished.m
@@ -1,229 +1,235 @@
 % CORE-forced run
+% with movement
 
-clear 
+clear
 close all
 
 %%
-cfile = 'Dc_enc70-b200_m4-b175-k086_c20-b250_D075_J100_A050_Sm025_nmort1_BE08_noCC_RE00100';
-fpath=['/Volumes/petrik-lab/Feisty/NC/Matlab_new_size/' cfile '/CORE/'];
+cfile = 'Dc_enc70-b200_m4-b175-k086_c20-b250_D075_J100_A050_Sm025_nmort1_BE08_CC80_RE00100';
+%fpath=['/Volumes/petrik-lab/Feisty/NC/Matlab_new_size/' cfile '/CORE/'];
+fpath=['/project/Feisty/NC/Matlab_new_size/' cfile '/CORE/'];
 
-exper = 'CORE_Hindcast_move_nu_v21_dt6h';
+mods = {'nomoveIC','spin50IC'};
 
-%% SP
-ncid = netcdf.open([fpath exper '_All_fish03_sml_p.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-[nid,nt] = size(biomass);
-SP.bio = biomass;
-
-clear biomass 
-
-%% SF
-ncid = netcdf.open([fpath exper '_All_fish03_sml_f.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-SF.bio = biomass(:,1:nt);
-
-clear biomass
-
-% SD
-ncid = netcdf.open([fpath exper '_All_fish03_sml_d.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-SD.bio = biomass;
-
-clear biomass 
-
-%% MP
-ncid = netcdf.open([fpath exper '_All_fish03_med_p.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-MP.bio = biomass;
-% MP.yield = yield;
-
-clear biomass yield
-
-% MF
-ncid = netcdf.open([fpath exper '_All_fish03_med_f.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-MF.bio = biomass;
-% MF.yield = yield;
-
-clear biomassyield
-
-% MD
-ncid = netcdf.open([fpath exper '_All_fish03_med_d.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-MD.bio = biomass;
-% MD.yield = yield;
-
-clear biomass yield
-
-% LP
-ncid = netcdf.open([fpath exper '_All_fish03_lrg_p.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-LP.bio = biomass;
-% LP.yield = yield;
-
-clear biomass yield
-
-% LD
-ncid = netcdf.open([fpath exper '_All_fish03_lrg_d.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-LD.bio = biomass;
-% LD.yield = yield;
-
-clear biomass yield
-
-% Benthic material
-ncid = netcdf.open([fpath exper '_All_fish03_bent.nc'],'NC_NOWRITE');
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-for i = 1:nvars
-    varname = netcdf.inqVar(ncid, i-1);
-    eval([ varname ' = netcdf.getVar(ncid,i-1);']);
-    eval([ varname '(' varname ' == 99999) = NaN;']);
-end
-netcdf.close(ncid);
-
-Bent.bio = biomass;
-BENT.bio = biomass(:,nt);
-clear biomass 
-
-%% Take means
-nt = length(time);
-MNTH = [31,28,31,30,31,30,31,31,30,31,30,31];
-
-
-%Time
-sp_tmean=mean(SP.bio,1,'omitnan');
-sf_tmean=mean(SF.bio,1,'omitnan');
-sd_tmean=mean(SD.bio,1,'omitnan');
-mp_tmean=mean(MP.bio,1,'omitnan');
-mf_tmean=mean(MF.bio,1,'omitnan');
-md_tmean=mean(MD.bio,1,'omitnan');
-lp_tmean=mean(LP.bio,1,'omitnan');
-ld_tmean=mean(LD.bio,1,'omitnan');
-b_tmean=mean(Bent.bio,1,'omitnan');
-
-%% Each year
-st=1:12:length(time);
-en=12:12:length(time);
-
-for n=1:length(st)
-    sp_smean(:,n)=mean(SP.bio(:,st(n):en(n)),2,'omitnan');
-    sf_smean(:,n)=mean(SF.bio(:,st(n):en(n)),2,'omitnan');
-    sd_smean(:,n)=mean(SD.bio(:,st(n):en(n)),2,'omitnan');
-    mp_smean(:,n)=mean(MP.bio(:,st(n):en(n)),2,'omitnan');
-    mf_smean(:,n)=mean(MF.bio(:,st(n):en(n)),2,'omitnan');
-    md_smean(:,n)=mean(MD.bio(:,st(n):en(n)),2,'omitnan');
-    lp_smean(:,n)=mean(LP.bio(:,st(n):en(n)),2,'omitnan');
-    ld_smean(:,n)=mean(LD.bio(:,st(n):en(n)),2,'omitnan');
-    b_smean(:,n)=mean(Bent.bio(:,st(n):en(n)),2,'omitnan');
+for m =1:length(mods)
     
-%     mp_my(:,n)=mean(MP.yield(:,st(n):en(n)),2,'omitnan');
-%     mf_my(:,n)=mean(MF.yield(:,st(n):en(n)),2,'omitnan');
-%     md_my(:,n)=mean(MD.yield(:,st(n):en(n)),2,'omitnan');
-%     lp_my(:,n)=mean(LP.yield(:,st(n):en(n)),2,'omitnan');
-%     ld_my(:,n)=mean(LD.yield(:,st(n):en(n)),2,'omitnan');
-end
+    exper = ['CORE_Hindcast_move_nu_v21_dt6h_All_fish03_' mods{m}];
 
-%% Whole time period mean
-sp_mean20=mean(SP.bio,2,'omitnan');
-sf_mean20=mean(SF.bio,2,'omitnan');
-sd_mean20=mean(SD.bio,2,'omitnan');
-mp_mean20=mean(MP.bio,2,'omitnan');
-mf_mean20=mean(MF.bio,2,'omitnan');
-md_mean20=mean(MD.bio,2,'omitnan');
-lp_mean20=mean(LP.bio,2,'omitnan');
-ld_mean20=mean(LD.bio,2,'omitnan');
-b_mean20=mean(Bent.bio,2,'omitnan');
+    %% SP
+    ncid = netcdf.open([fpath exper '_sml_p.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
 
-% mf_my20=mean(MF.yield,2);
-% mp_my20=mean(MP.yield,2);
-% md_my20=mean(MD.yield,2);
-% lp_my20=mean(LP.yield,2);
-% ld_my20=mean(LD.yield,2);
+    [nid,nt] = size(biomass);
+    SP.bio = biomass;
 
-%% Whole time period each month
-% sp_bio=SP.bio;
-% sf_bio=SF.bio;
-% sd_bio=SD.bio;
-% mp_bio=MP.bio;
-% mf_bio=MF.bio;
-% md_bio=MD.bio;
-% lp_bio=LP.bio;
-% ld_bio=LD.bio;
-% b_bio=Bent.bio;
-% 
-% mf_yield=MF.yield;
-% mp_yield=MP.yield;
-% md_yield=MD.yield;
-% lp_yield=LP.yield;
-% ld_yield=LD.yield;
+    clear biomass
 
-%% Save means
-save([fpath 'Means_' exper '_' cfile '.mat'],'time',...
-    'sf_smean','sp_smean','sd_smean',...
-    'mf_smean','mp_smean','md_smean',...
-    'b_smean','lp_smean','ld_smean',...
-    'sf_tmean','sp_tmean','sd_tmean',...
-    'mf_tmean','mp_tmean','md_tmean',...
-    'b_tmean','lp_tmean','ld_tmean',...
-    'sf_mean20','sp_mean20','sd_mean20',...
-    'mf_mean20','mp_mean20','md_mean20',...
-    'lp_mean20','ld_mean20','b_mean20'); %,...
+    %% SF
+    ncid = netcdf.open([fpath exper '_sml_f.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    SF.bio = biomass(:,1:nt);
+
+    clear biomass
+
+    % SD
+    ncid = netcdf.open([fpath exper '_sml_d.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    SD.bio = biomass;
+
+    clear biomass
+
+    %% MP
+    ncid = netcdf.open([fpath exper '_med_p.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    MP.bio = biomass;
+    % MP.yield = yield;
+
+    clear biomass yield
+
+    % MF
+    ncid = netcdf.open([fpath exper '_med_f.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    MF.bio = biomass;
+    % MF.yield = yield;
+
+    clear biomassyield
+
+    % MD
+    ncid = netcdf.open([fpath exper '_med_d.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    MD.bio = biomass;
+    % MD.yield = yield;
+
+    clear biomass yield
+
+    % LP
+    ncid = netcdf.open([fpath exper '_lrg_p.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    LP.bio = biomass;
+    % LP.yield = yield;
+
+    clear biomass yield
+
+    % LD
+    ncid = netcdf.open([fpath exper '_lrg_d.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    LD.bio = biomass;
+    % LD.yield = yield;
+
+    clear biomass yield
+
+    % Benthic material
+    ncid = netcdf.open([fpath exper '_bent.nc'],'NC_NOWRITE');
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    for i = 1:nvars
+        varname = netcdf.inqVar(ncid, i-1);
+        eval([ varname ' = netcdf.getVar(ncid,i-1);']);
+        eval([ varname '(' varname ' == 99999) = NaN;']);
+    end
+    netcdf.close(ncid);
+
+    Bent.bio = biomass;
+    BENT.bio = biomass(:,nt);
+    clear biomass
+
+    %% Take means
+    nt = length(time);
+    MNTH = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+
+    %Time
+    sp_tmean=mean(SP.bio,1,'omitnan');
+    sf_tmean=mean(SF.bio,1,'omitnan');
+    sd_tmean=mean(SD.bio,1,'omitnan');
+    mp_tmean=mean(MP.bio,1,'omitnan');
+    mf_tmean=mean(MF.bio,1,'omitnan');
+    md_tmean=mean(MD.bio,1,'omitnan');
+    lp_tmean=mean(LP.bio,1,'omitnan');
+    ld_tmean=mean(LD.bio,1,'omitnan');
+    b_tmean=mean(Bent.bio,1,'omitnan');
+
+    %% Each year
+    st=1:12:length(time);
+    en=12:12:length(time);
+
+    for n=1:length(st)
+        sp_smean(:,n)=mean(SP.bio(:,st(n):en(n)),2,'omitnan');
+        sf_smean(:,n)=mean(SF.bio(:,st(n):en(n)),2,'omitnan');
+        sd_smean(:,n)=mean(SD.bio(:,st(n):en(n)),2,'omitnan');
+        mp_smean(:,n)=mean(MP.bio(:,st(n):en(n)),2,'omitnan');
+        mf_smean(:,n)=mean(MF.bio(:,st(n):en(n)),2,'omitnan');
+        md_smean(:,n)=mean(MD.bio(:,st(n):en(n)),2,'omitnan');
+        lp_smean(:,n)=mean(LP.bio(:,st(n):en(n)),2,'omitnan');
+        ld_smean(:,n)=mean(LD.bio(:,st(n):en(n)),2,'omitnan');
+        b_smean(:,n)=mean(Bent.bio(:,st(n):en(n)),2,'omitnan');
+
+        %     mp_my(:,n)=mean(MP.yield(:,st(n):en(n)),2,'omitnan');
+        %     mf_my(:,n)=mean(MF.yield(:,st(n):en(n)),2,'omitnan');
+        %     md_my(:,n)=mean(MD.yield(:,st(n):en(n)),2,'omitnan');
+        %     lp_my(:,n)=mean(LP.yield(:,st(n):en(n)),2,'omitnan');
+        %     ld_my(:,n)=mean(LD.yield(:,st(n):en(n)),2,'omitnan');
+    end
+
+    %% Whole time period mean
+    sp_mean20=mean(SP.bio,2,'omitnan');
+    sf_mean20=mean(SF.bio,2,'omitnan');
+    sd_mean20=mean(SD.bio,2,'omitnan');
+    mp_mean20=mean(MP.bio,2,'omitnan');
+    mf_mean20=mean(MF.bio,2,'omitnan');
+    md_mean20=mean(MD.bio,2,'omitnan');
+    lp_mean20=mean(LP.bio,2,'omitnan');
+    ld_mean20=mean(LD.bio,2,'omitnan');
+    b_mean20=mean(Bent.bio,2,'omitnan');
+
+    % mf_my20=mean(MF.yield,2);
+    % mp_my20=mean(MP.yield,2);
+    % md_my20=mean(MD.yield,2);
+    % lp_my20=mean(LP.yield,2);
+    % ld_my20=mean(LD.yield,2);
+
+    %% Whole time period each month
+    % sp_bio=SP.bio;
+    % sf_bio=SF.bio;
+    % sd_bio=SD.bio;
+    % mp_bio=MP.bio;
+    % mf_bio=MF.bio;
+    % md_bio=MD.bio;
+    % lp_bio=LP.bio;
+    % ld_bio=LD.bio;
+    % b_bio=Bent.bio;
+    %
+    % mf_yield=MF.yield;
+    % mp_yield=MP.yield;
+    % md_yield=MD.yield;
+    % lp_yield=LP.yield;
+    % ld_yield=LD.yield;
+
+    %% Save means
+    save([fpath 'Means_' exper '_' cfile '.mat'],'time',...
+        'sf_smean','sp_smean','sd_smean',...
+        'mf_smean','mp_smean','md_smean',...
+        'b_smean','lp_smean','ld_smean',...
+        'sf_tmean','sp_tmean','sd_tmean',...
+        'mf_tmean','mp_tmean','md_tmean',...
+        'b_tmean','lp_tmean','ld_tmean',...
+        'sf_mean20','sp_mean20','sd_mean20',...
+        'mf_mean20','mp_mean20','md_mean20',...
+        'lp_mean20','ld_mean20','b_mean20'); %,...
     % 'mf_my20','mp_my20','md_my20',...
     % 'lp_my20','ld_my20',...
     % 'sf_bio','sp_bio','sd_bio',...
@@ -231,5 +237,5 @@ save([fpath 'Means_' exper '_' cfile '.mat'],'time',...
     % 'b_bio','lp_bio','ld_bio',...
     % 'mf_yield','mp_yield','md_yield',...
     % 'lp_yield','ld_yield');
-    
 
+end
--- CODE/advec/Advect_Fish_Code/postproc_mfiles/vis_spinup_core_move.m
+++ CODE/advec/Advect_Fish_Code/postproc_mfiles/vis_spinup_core_move.m
@@ -1,8 +1,8 @@
-% Visualize output of Spinup 
-% 50 years using no-movement spinup as ICs
+% Visualize output of Spinup
+% 150 years using IC = 1e-5
 % Saved as mat files
 
-clear 
+clear
 close all
 
 %%
@@ -17,10 +17,6 @@ if (~isfolder(ppath))
 end
 
 %%
-exper = 'Spinup1988_move_nu_v21_dt6h_All_fish03_';
-load([fpath 'Means_' exper cfile '.mat']);
-
-%%
 load('/Volumes/petrik-lab/Feisty/GCM_DATA/CORE-forced/ocean_cobalt_grid.mat',...
     'geolon_t','geolat_t');
 load('/Volumes/petrik-lab/Feisty/GCM_DATA/CORE-forced/Data_grid_ocean_cobalt_ESM2Mcore.mat',...
@@ -29,7 +25,7 @@ load('/Volumes/petrik-lab/Feisty/GCM_DATA/CORE-forced/Data_grid_ocean_cobalt_ESM
 [ni,nj]=size(geolon_t);
 geolon_t = double(geolon_t);
 geolat_t = double(geolat_t);
-plotminlat=-90; 
+plotminlat=-90;
 plotmaxlat=90;
 plotminlon=-280;
 plotmaxlon=80;
@@ -47,192 +43,209 @@ cm10=[0.5 0.5 0;... %tan/army
     0 0 0.75;...    %b
     0.5 0.5 0.5; ...    %med grey
     0 0 0];...      %black
-    
+
 
 set(groot,'defaultAxesColorOrder',cm10);
 
-%% Plots in time
-y = time/12;
-F = sf_tmean+mf_tmean;
-P = sp_tmean+mp_tmean+lp_tmean;
-D = sd_tmean+md_tmean+ld_tmean;
-B = b_tmean;
+%%
+behave = {'enc','nu','mort','prey'};
 
-% All size classes of all
-figure(1)
-plot(y,log10(b_tmean),'Linewidth',1); hold on;
-plot(y,log10(sf_tmean),'Linewidth',1); hold on;
-plot(y,log10(mf_tmean),'Linewidth',1); hold on;
-plot(y,log10(sp_tmean),'Linewidth',1); hold on;
-plot(y,log10(mp_tmean),'Linewidth',1); hold on;
-plot(y,log10(lp_tmean),'Linewidth',1); hold on;
-plot(y,log10(sd_tmean),'Linewidth',1); hold on;
-plot(y,log10(md_tmean),'Linewidth',1); hold on;
-plot(y,log10(ld_tmean),'Linewidth',1); hold on;
-legend('B','SF','MF','SP','MP','LP','SD','MD','LD')
-legend('location','eastoutside')
-xlim([y(1) y(end)])
-ylim([-3 2])
-xlabel('Time (mo)')
-ylabel('log10 Biomass (g m^-^2)')
-title('Spinup')
-stamp(exper)
-print('-dpng',[ppath exper 'all_sizes.png'])
+for b = 4 %1:length(behave)
 
-figure(2)
-plot(y,log10(B),'color',[0.5 0.5 0.5],'Linewidth',2); hold on;
-plot(y,log10(F),'r','Linewidth',2); hold on;
-plot(y,log10(P),'b','Linewidth',2); hold on;
-plot(y,log10(D),'k','Linewidth',2); hold on;
-legend('B','F','P','D')
-legend('location','eastoutside')
-xlim([y(1) y(end)])
-ylim([-2 2])
-xlabel('Time (y)')
-ylabel('log10 Biomass (g m^-^2)')
-title(['Spinup'])
-print('-dpng',[ppath exper 'all_types.png'])
+    %%
+    exper = ['Full_Spinup1988_move_',behave{b},'_v21_dt6h_All_fish03_'];
+    if b==4
+        exper2 = 'Full_Spinup1988_move_ingest_v21_dt6h_All_fish03_';
+    else
+        exper2 = exper;
+    end
 
-%% Plots in space
-Zsf=NaN*ones(ni,nj);
-Zsp=NaN*ones(ni,nj);
-Zsd=NaN*ones(ni,nj);
-Zmf=NaN*ones(ni,nj);
-Zmp=NaN*ones(ni,nj);
-Zmd=NaN*ones(ni,nj);
-Zlp=NaN*ones(ni,nj);
-Zld=NaN*ones(ni,nj);
-Zb=NaN*ones(ni,nj);
+    load([fpath 'Means_' exper2 cfile '.mat']);
 
-Zsf(GRD.ID)=sf_mean;
-Zsp(GRD.ID)=sp_mean;
-Zsd(GRD.ID)=sd_mean;
-Zmf(GRD.ID)=mf_mean;
-Zmp(GRD.ID)=mp_mean;
-Zmd(GRD.ID)=md_mean;
-Zlp(GRD.ID)=lp_mean;
-Zld(GRD.ID)=ld_mean;
-Zb(GRD.ID)=b_mean;
+    close all
 
-All = Zsp+Zsf+Zsd+Zmp+Zmf+Zmd+Zlp+Zld;
-AllF = Zsf+Zmf;
-AllP = Zsp+Zmp+Zlp;
-AllD = Zsd+Zmd+Zld;
-AllS = Zsp+Zsf+Zsd;
-AllM = Zmp+Zmf+Zmd;
-AllL = Zlp+Zld;
-FracPD = AllP ./ (AllP+AllD);
-FracPF = AllP ./ (AllP+AllF);
-FracLM = AllL ./ (AllL+AllM);
+    %% Plots in time
+    y = time/12;
+    F = sf_tmean+mf_tmean;
+    P = sp_tmean+mp_tmean+lp_tmean;
+    D = sd_tmean+md_tmean+ld_tmean;
+    B = b_tmean;
 
-%% bent
-figure(3)
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,log10(Zb))
-cmocean('dense')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([-2 2]);
-hcb = colorbar('h');
-set(gcf,'renderer','painters')
-title('log10 mean benthic biomass (g m^-^2)')
-stamp(exper)
-print('-dpng',[ppath exper 'global_BENT.png'])
+    % All size classes of all
+    figure(1)
+    plot(y,log10(b_tmean),'Linewidth',1); hold on;
+    plot(y,log10(sf_tmean),'Linewidth',1); hold on;
+    plot(y,log10(mf_tmean),'Linewidth',1); hold on;
+    plot(y,log10(sp_tmean),'Linewidth',1); hold on;
+    plot(y,log10(mp_tmean),'Linewidth',1); hold on;
+    plot(y,log10(lp_tmean),'Linewidth',1); hold on;
+    plot(y,log10(sd_tmean),'Linewidth',1); hold on;
+    plot(y,log10(md_tmean),'Linewidth',1); hold on;
+    plot(y,log10(ld_tmean),'Linewidth',1); hold on;
+    legend('B','SF','MF','SP','MP','LP','SD','MD','LD')
+    legend('location','eastoutside')
+    xlim([y(1) y(end)])
+    ylim([-3 2])
+    xlabel('Time (mo)')
+    ylabel('log10 Biomass (g m^-^2)')
+    title('Spinup')
+    stamp(exper2)
+    print('-dpng',[ppath exper2 'all_sizes.png'])
 
-%% ALL
-figure(4)
-% all F
-subplot('Position',[0 0.51 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,log10(AllF))
-cmocean('dense')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([-2 2]);
-colorbar('Position',[0.25 0.5 0.5 0.05],'orientation','horizontal')
-set(gcf,'renderer','painters')
-title('log10 mean All F (g m^-^2)')
+    figure(2)
+    plot(y,log10(B),'color',[0.5 0.5 0.5],'Linewidth',2); hold on;
+    plot(y,log10(F),'r','Linewidth',2); hold on;
+    plot(y,log10(P),'b','Linewidth',2); hold on;
+    plot(y,log10(D),'k','Linewidth',2); hold on;
+    legend('B','F','P','D')
+    legend('location','eastoutside')
+    xlim([y(1) y(end)])
+    ylim([-2 2])
+    xlabel('Time (y)')
+    ylabel('log10 Biomass (g m^-^2)')
+    title(['Spinup'])
+    print('-dpng',[ppath exper2 'all_types.png'])
 
-% all D
-subplot('Position',[0 0 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,log10(AllD))
-cmocean('dense')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([-2 2]);
-set(gcf,'renderer','painters')
-title('log10 mean All D (g m^-^2)')
+    %% Plots in space
+    Zsf=NaN*ones(ni,nj);
+    Zsp=NaN*ones(ni,nj);
+    Zsd=NaN*ones(ni,nj);
+    Zmf=NaN*ones(ni,nj);
+    Zmp=NaN*ones(ni,nj);
+    Zmd=NaN*ones(ni,nj);
+    Zlp=NaN*ones(ni,nj);
+    Zld=NaN*ones(ni,nj);
+    Zb=NaN*ones(ni,nj);
 
-% All P
-subplot('Position',[0.5 0.51 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,log10(AllP))
-cmocean('dense')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([-2 2]);
-set(gcf,'renderer','painters')
-title('log10 mean All P (g m^-^2)')
+    Zsf(GRD.ID)=sf_mean;
+    Zsp(GRD.ID)=sp_mean;
+    Zsd(GRD.ID)=sd_mean;
+    Zmf(GRD.ID)=mf_mean;
+    Zmp(GRD.ID)=mp_mean;
+    Zmd(GRD.ID)=md_mean;
+    Zlp(GRD.ID)=lp_mean;
+    Zld(GRD.ID)=ld_mean;
+    Zb(GRD.ID)=b_mean;
 
-% All
-subplot('Position',[0.5 0 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,log10(All))
-cmocean('dense')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([-2 2]);
-set(gcf,'renderer','painters')
-title('log10 mean All fishes (g m^-^2)')
-stamp(exper)
-print('-dpng',[ppath exper 'All_subplot.png'])
+    All = Zsp+Zsf+Zsd+Zmp+Zmf+Zmd+Zlp+Zld;
+    AllF = Zsf+Zmf;
+    AllP = Zsp+Zmp+Zlp;
+    AllD = Zsd+Zmd+Zld;
+    AllS = Zsp+Zsf+Zsd;
+    AllM = Zmp+Zmf+Zmd;
+    AllL = Zlp+Zld;
+    FracPD = AllP ./ (AllP+AllD);
+    FracPF = AllP ./ (AllP+AllF);
+    FracLM = AllL ./ (AllL+AllM);
 
-%% Ratios on subplots 3 figure subplot P:D, P:F, M:L
-figure(5)
-subplot('Position',[0 0.53 0.5 0.5])
-%P:D
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,FracPD)
-cmocean('balance')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([0 1]);
-set(gcf,'renderer','painters')
-title('Fraction Large Pelagics vs. Demersals')
+    %% bent
+    figure(3)
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,log10(Zb))
+    cmocean('dense')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([-2 2]);
+    hcb = colorbar('h');
+    set(gcf,'renderer','painters')
+    title('log10 mean benthic biomass (g m^-^2)')
+    stamp(exper2)
+    print('-dpng',[ppath exper2 'global_BENT.png'])
 
-%P:F
-subplot('Position',[0.5 0.53 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,FracPF)
-cmocean('balance')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([0 1]);
-set(gcf,'renderer','painters')
-title('Fraction Large Pelagics vs. Forage Fishes')
+    %% ALL
+    figure(4)
+    % all F
+    subplot('Position',[0 0.51 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,log10(AllF))
+    cmocean('dense')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([-2 2]);
+    colorbar('Position',[0.25 0.5 0.5 0.05],'orientation','horizontal')
+    set(gcf,'renderer','painters')
+    title('log10 mean All F (g m^-^2)')
 
-%L:M
-subplot('Position',[0.25 0.0 0.5 0.5])
-axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
-    'Grid','off','FLineWidth',1,'origin',[0 -100 0])
-surfm(geolat_t,geolon_t,FracLM)
-cmocean('balance')
-load coastlines;                     
-h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
-clim([0 1]);
-colorbar('Position',[0.2 0.485 0.6 0.05],'orientation','horizontal')
-set(gcf,'renderer','painters')
-title('Fraction Large vs. Medium')
-stamp(exper)
-print('-dpng',[ppath exper 'global_ratios_subplot.png'])
+    % all D
+    subplot('Position',[0 0 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,log10(AllD))
+    cmocean('dense')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([-2 2]);
+    set(gcf,'renderer','painters')
+    title('log10 mean All D (g m^-^2)')
 
+    % All P
+    subplot('Position',[0.5 0.51 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,log10(AllP))
+    cmocean('dense')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([-2 2]);
+    set(gcf,'renderer','painters')
+    title('log10 mean All P (g m^-^2)')
 
+    % All
+    subplot('Position',[0.5 0 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,log10(All))
+    cmocean('dense')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([-2 2]);
+    set(gcf,'renderer','painters')
+    title('log10 mean All fishes (g m^-^2)')
+    stamp(exper2)
+    print('-dpng',[ppath exper2 'All_subplot.png'])
+
+    %% Ratios on subplots 3 figure subplot P:D, P:F, M:L
+    figure(5)
+    subplot('Position',[0 0.53 0.5 0.5])
+    %P:D
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,FracPD)
+    cmocean('balance')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([0 1]);
+    set(gcf,'renderer','painters')
+    title('Fraction Large Pelagics vs. Demersals')
+
+    %P:F
+    subplot('Position',[0.5 0.53 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,FracPF)
+    cmocean('balance')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([0 1]);
+    set(gcf,'renderer','painters')
+    title('Fraction Large Pelagics vs. Forage Fishes')
+
+    %L:M
+    subplot('Position',[0.25 0.0 0.5 0.5])
+    axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+        'Grid','off','FLineWidth',1,'origin',[0 -100 0])
+    surfm(geolat_t,geolon_t,FracLM)
+    cmocean('balance')
+    load coastlines;
+    h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+    clim([0 1]);
+    colorbar('Position',[0.2 0.485 0.6 0.05],'orientation','horizontal')
+    set(gcf,'renderer','painters')
+    title('Fraction Large vs. Medium')
+    stamp(exper2)
+    print('-dpng',[ppath exper2 'global_ratios_subplot.png'])
+
+end
 
