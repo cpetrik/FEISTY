4157e80ace8b8ae93c1f582e442e5ccacb3ca5e8
Colleen <cpetrik@users.noreply.github.com>
2026-02-17 15:55:19 -0800

    Interp integrated global offline forcing

--- CODE/vertical/vert_int_forcing_multyr.m
+++ CODE/vertical/vert_int_forcing_multyr.m
@@ -4,8 +4,8 @@ clear
 close all
 
 %%
-vpath = '/Volumes/petrik-lab/Feisty/GCM_Data/OM4_05_COBALTv3_FEISTYoff/';
-%vpath = '/project/Feisty/GCM_Data/OM4_05_COBALTv3_FEISTYoff/';
+%vpath = '/Volumes/petrik-lab/Feisty/GCM_Data/OM4_05_COBALTv3_FEISTYoff/';
+vpath = '/project/Feisty/GCM_Data/OM4_05_COBALTv3_FEISTYoff/';
 
 %1-D
 load([vpath 'grid_OM4_05_COBALTv3.mat'],'wet','geolon','z_l');
@@ -26,19 +26,25 @@ st = 1:12:60;
 yst = 1990:5:2019;
 yen = 1994:5:2019;
 
+allY = 1990:2019;
+ytick = 0;
 for Y = 1:length(yst)
-    %% read in one year
-    yrs = [num2str(yst(Y)),'01-',num2str(yen(Y)),'12'];
-    [TEMP_z,TEMP_btm,MZ,LZ,MZloss,LZloss,det_btm] = ncread_global_feisty_forcing_1year(vpath,ni,nj,NZID,yrs,styr);
+    for m = 1:5
+        %% read in one year
+        ytick= ytick+1;
+        yrs = [num2str(yst(Y)),'01-',num2str(yen(Y)),'12'];
+        styr = st(m);
+        [TEMP_z,TEMP_btm,MZ,LZ,MZloss,LZloss,det_btm] = ncread_global_feisty_forcing_1year(vpath,ni,nj,NZID,yrs,styr);
 
-    % THICKNESS
-    load([vpath 'ocean_cobalt_feisty_forcing_z.',yrs,'.thkcello.mat'],'thkcello')
-    thkcello = thkcello(:,:,:,1:12);
+        % THICKNESS
+        load([vpath 'ocean_cobalt_feisty_forcing_z.',yrs,'.thkcello.mat'],'thkcello')
+        thkcello = thkcello(:,:,:,styr:(styr+11));
 
-    %% Interpolate monthly forcing to daily
-    ESM = daily_interp_int_monthly_means(NWID,Time,Tdays,...
-        TEMP_z,TEMP_btm,det_btm,MZ,LZ,MZloss,LZloss,thkcello,WID,ni,nj);
+        %% Interpolate monthly forcing to daily
+        ESM = daily_interp_int_monthly_means(NWID,Time,Tdays,...
+            TEMP_z,TEMP_btm,det_btm,MZ,LZ,MZloss,LZloss,thkcello,WID,ni,nj);
 
-    %% Save
-    save([vpath 'ocean_cobalt_feisty_forcing_2dint.1990.mat'],'ESM','-v7.3')
+        %% Save
+        save([vpath 'ocean_cobalt_feisty_forcing_2dint.',num2str(allY(ytick)),'.mat'],'ESM','-v7.3')
+    end
 end
